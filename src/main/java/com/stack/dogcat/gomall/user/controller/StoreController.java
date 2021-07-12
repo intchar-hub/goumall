3abe52bb76859e412dc17e8b6c2a748cedde286a
Yang Jie <1453370940@qq.com>
2021-07-12 20:35:42 +0800

    接口1.1.4-1.1.8 商家密码登录； 商家邮箱验证码登录； 商家修改个人信息； 商家获取个人信息； 商家删除账号

--- src/main/java/com/stack/dogcat/gomall/user/controller/StoreController.java
+++ src/main/java/com/stack/dogcat/gomall/user/controller/StoreController.java
@@ -3,6 +3,9 @@ package com.stack.dogcat.gomall.user.controller;
 
 import com.stack.dogcat.gomall.commonResponseVo.SysResult;
 import com.stack.dogcat.gomall.user.requestVo.StoreRegisterRequestVo;
+import com.stack.dogcat.gomall.user.requestVo.StoreUpdateInfoRequestVo;
+import com.stack.dogcat.gomall.user.responseVo.StoreInfoQueryResponseVo;
+import com.stack.dogcat.gomall.user.responseVo.StoreLoginResponseVo;
 import com.stack.dogcat.gomall.user.service.IStoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import java.io.IOException;
 
 /**
  * <p>
@@ -29,7 +31,7 @@ public class StoreController {
     IStoreService storeService;
 
     /**
-     * 商家获取图片验证码
+     * 商家获取字符验证码
      * @param request
      * @param response
      */
@@ -72,9 +74,95 @@ public class StoreController {
             storeService.register(request, registerRequestVo);
         } catch (Exception e) {
             System.out.println(e);
-            return SysResult.error("注册失败");
+            return SysResult.error(e.getMessage());
         }
         return SysResult.success();
     }
 
+    /**
+     * 商家密码登录
+     * @param userName
+     * @param password
+     * @param verifyString
+     * @return
+     */
+    @PostMapping("/pwdLogin")
+    public SysResult pwdLogin(HttpServletRequest request, String userName, String password, String verifyString) {
+        StoreLoginResponseVo responseVo = null;
+        try {
+            responseVo= storeService.pwdLogin(request, userName, password, verifyString);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return SysResult.error(e.getMessage());
+        }
+        return SysResult.success(responseVo);
+    }
+
+    /**
+     * 商家邮箱验证码登录
+     * @param email
+     * @param verifyCode
+     * @return
+     */
+    @PostMapping("/emailLogin")
+    public SysResult emailLogin(HttpServletRequest request, String email, String verifyCode) {
+        StoreLoginResponseVo responseVo = null;
+        try {
+            responseVo = storeService.emailLogin(request, email, verifyCode);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return SysResult.error(e.getMessage());
+        }
+        return SysResult.success(responseVo);
+    }
+
+    /**
+     * 商家更新个人/店铺信息
+     * @param requestVo
+     * @return
+     */
+    @PostMapping("/updateStoreInfo")
+    public SysResult updateStoreInfo(@RequestBody StoreUpdateInfoRequestVo requestVo) {
+        try {
+            storeService.updateStoreInfo(requestVo);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return SysResult.error("更新失败");
+        }
+        return SysResult.success();
+    }
+
+    /**
+     * 获取商家信息
+     * @param id
+     * @return
+     */
+    @GetMapping("/getStoreInfo")
+    public SysResult getStoreInfo(Integer id) {
+        StoreInfoQueryResponseVo responseVo = null;
+        try {
+            responseVo = storeService.getStoreInfo(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return SysResult.error(e.getMessage());
+        }
+        return SysResult.success(responseVo);
+    }
+
+    /**
+     * 商家删除/注销账号
+     * @param id
+     * @param password
+     * @return
+     */
+    @DeleteMapping("/deleteStore")
+    public SysResult deleteStore(Integer id, String password) {
+        try {
+            storeService.deleteStore(id, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return SysResult.error(e.getMessage());
+        }
+        return SysResult.success();
+    }
 }
