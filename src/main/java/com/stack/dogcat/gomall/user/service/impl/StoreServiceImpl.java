575d705613ba43350f44e11d3bc398888dfc81a2
Yang Jie <1453370940@qq.com>
2021-07-12 14:10:43 +0800

    修改统一返回类SysResult； 添加邮箱配置； 编写商家获取字符验证码和邮箱验证码接口;

--- src/main/java/com/stack/dogcat/gomall/user/service/impl/StoreServiceImpl.java
+++ src/main/java/com/stack/dogcat/gomall/user/service/impl/StoreServiceImpl.java
@@ -4,7 +4,12 @@ import com.stack.dogcat.gomall.user.entity.Store;
 import com.stack.dogcat.gomall.user.mapper.StoreMapper;
 import com.stack.dogcat.gomall.user.service.IStoreService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 /**
  * <p>
@@ -17,4 +22,23 @@ import org.springframework.stereotype.Service;
 @Service
 public class StoreServiceImpl extends ServiceImpl<StoreMapper, Store> implements IStoreService {
 
+    @Autowired
+    JavaMailSenderImpl mailSender;
+
+    private String emailServiceCode;
+
+    /**
+     * 向商家发送邮箱验证码（四位数）
+     * @param email
+     */
+    @Override
+    public void sendEmailCode(String email) {
+        emailServiceCode = String.format("%04d", new Random().nextInt(9999));
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setSubject("注册验证码");
+        message.setText("验证码：" + emailServiceCode);
+        message.setFrom("1453370940@qq.com");
+        message.setTo(email);
+        mailSender.send(message);
+    }
 }
