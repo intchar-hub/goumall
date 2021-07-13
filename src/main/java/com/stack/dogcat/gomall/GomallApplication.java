4a7ca235a79b882d59958dd2d0d167c88d7528a6
xzk9527 <2391529915@qq.com>
2021-07-13 10:42:25 +0800

    解决RestTemplate无法注入

--- src/main/java/com/stack/dogcat/gomall/GomallApplication.java
+++ src/main/java/com/stack/dogcat/gomall/GomallApplication.java
@@ -1,8 +1,15 @@
 package com.stack.dogcat.gomall;
 
+import com.stack.dogcat.gomall.utils.CurrentUserHandlerMethodArgReslover;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+
+
+import java.util.List;
 
 @SpringBootApplication
 @MapperScan("com.stack.dogcat.gomall.*.mapper")
@@ -11,5 +18,15 @@ public class GomallApplication {
     public static void main(String[] args) {
         SpringApplication.run(GomallApplication.class, args);
     }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+
+
+
+
 
 }
