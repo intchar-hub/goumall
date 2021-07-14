5d364d85083683cf880338a85a86d8e179ffa133
xzk9527 <2391529915@qq.com>
2021-07-14 15:57:33 +0800

    收货地址接口

--- src/main/java/com/stack/dogcat/gomall/order/controller/ReceiveAddressController.java
+++ src/main/java/com/stack/dogcat/gomall/order/controller/ReceiveAddressController.java
@@ -1,9 +1,18 @@
 package com.stack.dogcat.gomall.order.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.stack.dogcat.gomall.annotation.CurrentUser;
+import com.stack.dogcat.gomall.annotation.Token;
+import com.stack.dogcat.gomall.commonResponseVo.SysResult;
+import com.stack.dogcat.gomall.order.entity.ReceiveAddress;
+import com.stack.dogcat.gomall.order.responseVo.ReceiveAddressQueryResponseVo;
+import com.stack.dogcat.gomall.order.service.IOrderService;
+import com.stack.dogcat.gomall.order.service.IReceiveAddressService;
+import com.stack.dogcat.gomall.user.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +25,84 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/order/receive-address")
 public class ReceiveAddressController {
+
+    @Autowired
+    IReceiveAddressService receiveAddressService;
+
+
+    /**
+     * 用户添加收货地址
+     * @param current_customer
+     * @param address
+     * @return
+     */
+    @PostMapping("/saveReceiveAddress")
+    @ResponseBody
+    @Token.UserLoginToken
+    public SysResult saveReceiveAddress(@CurrentUser Customer current_customer, String address)
+    {
+        try{
+            Integer customerId =current_customer.getId();
+            receiveAddressService.insertReceiveAddressByCustomerId(customerId,address);
+            SysResult result =SysResult.success();
+            return  result;
+        }catch (Exception e){
+            SysResult result=SysResult.error(e.getMessage());
+            return result;
+        }
+    }
+
+
+    /**
+     * 用户修改收货地址
+     * @param current_customer
+     * @param receiveAddressId
+     * @param address
+     * @param defaultAddress
+     * @return
+     */
+    @PostMapping("/updateReceiveAddress")
+    @ResponseBody
+    @Token.UserLoginToken
+    public SysResult updateReceiveAddress(@CurrentUser Customer current_customer,Integer receiveAddressId,String address,Integer defaultAddress){
+
+        //Integer customerId =current_customer.getId();
+        try{
+            ReceiveAddress receiveAddress=receiveAddressService.queryReceiveAddressByReceiveAddressId(receiveAddressId);
+            if(address!=null){
+                receiveAddress.setAddress(address);
+            }
+            if(defaultAddress!=null){
+                receiveAddress.setDefaultAddress(defaultAddress);
+            }
+            receiveAddressService.updateReceiveAddressById(receiveAddress);
+            SysResult result =SysResult.success();
+            return result;
+        }catch (Exception e){
+            SysResult result =SysResult.error(e.getMessage());
+            return result;
+        }
+    }
+
+
+    /**
+     * 用户查询收货地址
+     * @param current_customer
+     * @return
+     */
+    @GetMapping("/listReceiveAddress")
+    @ResponseBody
+    @Token.UserLoginToken
+    public SysResult queryListReceiveAddress(@CurrentUser Customer current_customer){
+        try{
+            Integer customerId = current_customer.getId();
+            List<ReceiveAddressQueryResponseVo> receiveAddressQueryResponseVos=receiveAddressService.queryReceiveAddressByCustomerId(customerId);
+            SysResult result = SysResult.success(receiveAddressQueryResponseVos);
+            return result;
+        }catch (Exception e){
+            SysResult result=SysResult.error(e.getMessage());
+            return result;
+        }
+    }
 
 }
