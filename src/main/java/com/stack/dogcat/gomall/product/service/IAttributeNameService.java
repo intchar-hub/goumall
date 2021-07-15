c2b70ee7c136ba4886641bd0155e103ecdf318fd
xzk9527 <2391529915@qq.com>
2021-07-15 19:17:23 +0800

    添加属性接口

--- src/main/java/com/stack/dogcat/gomall/product/service/IAttributeNameService.java
+++ src/main/java/com/stack/dogcat/gomall/product/service/IAttributeNameService.java
@@ -3,6 +3,8 @@ package com.stack.dogcat.gomall.product.service;
 import com.stack.dogcat.gomall.product.entity.AttributeName;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.ArrayList;
+
 /**
  * <p>
  * 属性名 服务类
@@ -13,4 +15,9 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IAttributeNameService extends IService<AttributeName> {
 
+    //插入属性名(手工录入)
+    public void insertAttributeNameManualInput(AttributeName attributeName);
+
+    //插入属性名，属性值列表
+    public void insertAttributeNameAndValueList(AttributeName attributeName, ArrayList<String> valueArray);
 }
--- src/main/java/com/stack/dogcat/gomall/product/service/impl/AttributeNameServiceImpl.java
+++ src/main/java/com/stack/dogcat/gomall/product/service/impl/AttributeNameServiceImpl.java
@@ -1,10 +1,19 @@
 package com.stack.dogcat.gomall.product.service.impl;
 
+import com.stack.dogcat.gomall.product.entity.AttributeCollection;
 import com.stack.dogcat.gomall.product.entity.AttributeName;
+import com.stack.dogcat.gomall.product.entity.AttributeValue;
+import com.stack.dogcat.gomall.product.mapper.AttributeCollectionMapper;
 import com.stack.dogcat.gomall.product.mapper.AttributeNameMapper;
+import com.stack.dogcat.gomall.product.mapper.AttributeValueMapper;
 import com.stack.dogcat.gomall.product.service.IAttributeNameService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 /**
  * <p>
@@ -16,5 +25,49 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class AttributeNameServiceImpl extends ServiceImpl<AttributeNameMapper, AttributeName> implements IAttributeNameService {
+
+    @Autowired
+    AttributeNameMapper attributeNameMapper;
+
+    @Autowired
+    AttributeCollectionMapper attributeCollectionMapper;
+
+    @Autowired
+    AttributeValueMapper attributeValueMapper;
+
+    @Override
+    @Transactional
+    public void insertAttributeNameManualInput(AttributeName attributeName){
+
+        attributeNameMapper.insert(attributeName);
+        //属性集合中属性数量+1
+        AttributeCollection attributeCollection = attributeCollectionMapper.selectById(attributeName.getAttributeCollectionId());
+        Integer attributeNum=attributeCollection.getAttributeNum()+1;
+        attributeCollection.setAttributeNum(attributeNum);
+        attributeCollectionMapper.updateById(attributeCollection);
+
+    }
+
+    @Override
+    @Transactional
+    public  void insertAttributeNameAndValueList(AttributeName attributeName, ArrayList<String> valueArray){
+
+        attributeNameMapper.insert(attributeName);
+        //添加属性值列表
+        Integer attributeNameId=attributeName.getId();
+        for(int i=0;i<valueArray.size();i++){
+            AttributeValue attributeValue =new AttributeValue();
+            attributeValue.setAttributeNameId(attributeNameId);
+            attributeValue.setValue(valueArray.get(i));
+            attributeValue.setGmtCreate(LocalDateTime.now());
+            attributeValueMapper.insert(attributeValue);
+        }
+        //属性集合中属性数量+1
+        AttributeCollection attributeCollection = attributeCollectionMapper.selectById(attributeName.getAttributeCollectionId());
+        Integer attributeNum=attributeCollection.getAttributeNum()+1;
+        attributeCollection.setAttributeNum(attributeNum);
+        attributeCollectionMapper.updateById(attributeCollection);
+
+    }
 
 }
