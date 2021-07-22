407012f63c94ca901d96d12583f4092f5daa96a2
Yang Jie <1453370940@qq.com>
2021-07-23 00:18:58 +0800

    定时更新商品的is_onsale字段； 添加查看参加秒杀活动的商品接口； 商家登录时判断是否审核通过

--- src/main/java/com/stack/dogcat/gomall/sales/service/ISalesPromotionService.java
+++ src/main/java/com/stack/dogcat/gomall/sales/service/ISalesPromotionService.java
@@ -4,6 +4,7 @@ import com.stack.dogcat.gomall.commonResponseVo.PageResponseVo;
 import com.stack.dogcat.gomall.sales.entity.SalesPromotion;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.stack.dogcat.gomall.sales.requestVo.SalesPromotionSaveRequestVo;
+import com.stack.dogcat.gomall.sales.responseVo.SalesProductQueryResponseVo;
 import com.stack.dogcat.gomall.sales.responseVo.SalesPromotionQueryResponseVo;
 
 import java.util.List;
@@ -23,4 +24,6 @@ public interface ISalesPromotionService extends IService<SalesPromotion> {
     PageResponseVo<SalesPromotionQueryResponseVo> listPromotionByStore(Integer pageNum, Integer pageSize, Integer storeId, Integer screenCondition);
 
     List<SalesPromotionQueryResponseVo> listPromotion(Integer screenCondition);
+
+    List<SalesProductQueryResponseVo> listPromotionProducts();
 }
