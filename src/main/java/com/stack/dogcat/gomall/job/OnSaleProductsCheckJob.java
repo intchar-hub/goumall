9e599a8b6e75c78e30b63842942ed60f4638deb5
Yang Jie <1453370940@qq.com>
2021-07-27 08:54:42 +0800

    检查秒杀活动是否过期定时任务：更新秒杀活动表中的status字段

--- src/main/java/com/stack/dogcat/gomall/job/OnSaleProductsCheckJob.java
+++ src/main/java/com/stack/dogcat/gomall/job/OnSaleProductsCheckJob.java
@@ -1,6 +1,7 @@
 package com.stack.dogcat.gomall.job;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.stack.dogcat.gomall.product.entity.Product;
 import com.stack.dogcat.gomall.product.mapper.ProductMapper;
 import com.stack.dogcat.gomall.sales.entity.SalesPromotion;
@@ -8,6 +9,7 @@ import com.stack.dogcat.gomall.sales.mapper.SalesPromotionMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -27,14 +29,24 @@ public class OnSaleProductsCheckJob {
     SalesPromotionMapper salesPromotionMapper;
 
     /**
-     * 判断商品是否处于秒杀活动中
+     * 判断商品是否处于秒杀活动中、秒杀活动是否过期
      */
     @Scheduled(cron= "0 0/5 * * * ? ")
+    @Transactional
     public void checkJobOnSaleProducts(){
         QueryWrapper queryWrapper = new QueryWrapper();
         queryWrapper.lt("deadline", LocalDateTime.now());
         List<SalesPromotion> salesPromotionsDB = salesPromotionMapper.selectList(queryWrapper);
         for (SalesPromotion salesPromotion : salesPromotionsDB) {
+            /**
+             * 更新秒杀活动表中的status字段
+             */
+            salesPromotion.setStatus(1);
+            salesPromotionMapper.updateById(salesPromotion);
+
+            /**
+             * 更新商品表中的is_onsale字段
+             */
             Product productDB = productMapper.selectById(salesPromotion.getProductId());
             if(productDB.getStatus() == 1) {
                 productDB.setIsOnsale(0);
